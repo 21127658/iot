d51ca3fe89c25424adc132902a6aba3175b7ed80
2eggsnoodles <thmnhat21@clc.fitus.edu.vn>
2024-10-17 19:12:52 +0700

    Completed reading info from Serial monitor

--- IoT.py
+++ IoT.py
@@ -2,49 +2,47 @@ import serial
 import threading
 import flask
 from flask import render_template
+import time
+import datetime
 
 def readingThread():
     global is_reading
-    with serial.Serial('COM7', 9600, timeout=1) as ser:
+    with serial.Serial('COM7', 9600, timeout = 2) as ser:
         while is_reading:
             line = ser.readline()
             update_data(line.strip())
-            
+        ser.close()
+
 def update_data(s):
     global data
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
     try:
-        data = int(s)
+        data = time_string + " (Brightness | Temperature(oC) | Humidity(%)): " + str(s)[2:-1]
         return True
-    except:
+    except ValueError:
         print("Invalid data: ", s)
         return False
 
 app = flask.Flask(__name__)
-@app.route('/')
-def hello():
-    return render_template('/templates/view.html')
 
-state = 'initiate'
-@app.route('/cmd', methods = ["GET"])
-def cmd():
-    global state
-    new_state = flask.request.args.get('state')
-    if new_state:
-        state = new_state
-    return f"<p><strong>State: {state}</strong></p>"
+@app.route('/')
+def auto():
+    global data
+    return data
 
 # Turn off serial monitor on Arduino IDE before running this script
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
-    global is_reading
+    tmp = 1
+    global is_reading 
     is_reading = True
     global data
-    data = 0
+    data = ""
     t = threading.Thread(target=readingThread)
     t.start()
+    app.run(host='0.0.0.0')
     while True:
-        print(data)
-        if data > 100:
+        if tmp > 100:
             is_reading = False
             break
-    
+        tmp += 1
